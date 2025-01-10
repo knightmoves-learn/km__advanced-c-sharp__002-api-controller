7af1bfb29bc45bf0504ed71a1e244976876f4b84
ChrisAndersonKM <chris.anderson@knightmoves.org>
2025-01-10 16:13:14 -0600

    more QA fixes

--- StockPriceApi/Program.cs
+++ StockPriceApi/Program.cs
@@ -4,9 +4,9 @@ var app = builder.Build();
 string[] stockTickers = { "AAPL", "GOOGL", "MSFT", "AMZN", "META", "TSLA", "NVDA", "JPM", "WMT", "KO" };
 decimal[] stockPrices = { 173.50m, 147.23m, 401.89m, 177.23m, 495.27m, 185.10m, 817.49m, 146.75m, 59.93m, 60.45m };
 
-app.MapGet("/StockPrices", () =>
+app.MapGet("/StockPrice", () =>
 {
-    var prices =  Enumerable.Range(0, 9).Select(index =>
+    var prices = Enumerable.Range(0, 9).Select(index =>
         new StockPrices
         (
             stockTickers[index],
@@ -20,4 +20,4 @@ app.MapGet("/StockPrices", () =>
 
 app.Run();
 
-record StockPrices(string stockTicker, decimal stockPrice);
+public record StockPrices(string stockTicker, decimal stockPrice);
