abdd58338192ccacb4b31e3c58fa80b9ebb3543a
Isaac <isaac@knightmoves.org>
2025-01-10 12:02:50 -0600

    QA

---
+++ StockPriceApi/Controllers/StockPriceController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockPriceApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class StockPriceController : ControllerBase
+    {
+        private string[] stockTickers = new[]
+        { 
+            "AAPL", "GOOGL", "MSFT", "AMZN", "META", "TSLA", "NVDA", "JPM", "WMT", "KO" 
+        };
+
+        private decimal[] stockPrices = new[]
+        {
+            173.50m, 147.23m, 401.89m, 177.23m, 495.27m, 185.10m, 817.49m, 146.75m, 59.93m, 60.45m 
+        };
+
+        [HttpGet]
+        private IEnumerable<StockPrices> Get()
+        {
+            var prices =  Enumerable.Range(0, 9).Select(index => new StockPrices
+            (
+                stockTickers[index],
+                stockPrices[index]
+            )).ToArray();
+            return prices;
+        }
+
+    }
+}
--- StockPriceApi/Program.cs
+++ StockPriceApi/Program.cs
@@ -1,22 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 var app = builder.Build();
-
-string[] stockTickers = { "AAPL", "GOOGL", "MSFT", "AMZN", "META", "TSLA", "NVDA", "JPM", "WMT", "KO" };
-decimal[] stockPrices = { 173.50m, 147.23m, 401.89m, 177.23m, 495.27m, 185.10m, 817.49m, 146.75m, 59.93m, 60.45m };
-
-app.MapGet("/StockPrices", () =>
-{
-    var prices =  Enumerable.Range(0, 9).Select(index =>
-        new StockPrices
-        (
-            stockTickers[index],
-            stockPrices[index]
-        ))
-        .ToArray();
-    return prices;
-})
-.WithName("GetStockPrices")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
